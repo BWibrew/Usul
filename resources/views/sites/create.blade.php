0dd0f7053014d6eee168918b22d854759055d3b4
Ben Wibrew <benjamin.wibrew@gmail.com>
2017-11-10 20:41:56 +0000

    Add create blade template

---
+++ resources/views/sites/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <div class="card">
+                    <div class="card-header">New Site</div>
+
+                    <div class="card-body">
+                        <p>Hello World!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
