ca3ee9813bdae714d774e6fd442a7271364bb26f
Ben Wibrew <benjamin.wibrew@gmail.com>
2018-02-24 13:08:54 +0000

    Seperate model factories into separate files

---
+++ database/factories/SitesFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/** @var $factory \Illuminate\Database\Eloquent\Factory */
+$factory->define(App\Site::class, function (Faker $faker) {
+    return [
+        'name' => $faker->words(3, true),
+        'url' => $url = $faker->url,
+        'root_uri' => $url.$faker->slug(),
+        'namespaces' => json_encode([]),
+    ];
+});
