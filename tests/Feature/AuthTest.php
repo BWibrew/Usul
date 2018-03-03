ca01a87cbd54ef6a5059b0624f16ef34cbb9a547
Ben Wibrew <benjamin.wibrew@gmail.com>
2018-03-03 19:54:45 +0000

    Improve tests

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -4,6 +4,9 @@ namespace Tests\Feature;
 
 use App\User;
 use Tests\TestCase;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Hashing\BcryptHasher;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Auth\Notifications\ResetPassword;
@@ -14,7 +17,7 @@ class AuthTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function it_loads_login_form()
+    public function it_shows_login_form()
     {
         $this->get('login')
              ->assertStatus(200)
@@ -22,7 +25,7 @@ class AuthTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_log_in()
+    public function it_redirects_user_to_sites_after_login()
     {
         $user = factory(User::class)->create();
 
@@ -58,7 +61,7 @@ class AuthTest extends TestCase
     }
 
     /** @test */
-    public function it_loads_register_form()
+    public function it_shows_register_form()
     {
         $this->get('register')
              ->assertStatus(200)
@@ -66,7 +69,7 @@ class AuthTest extends TestCase
     }
 
     /** @test */
-    public function it_can_register_a_new_user()
+    public function it_redirects_user_to_sites_after_registering()
     {
         $this->post('register', [
             'name' => 'name',
@@ -83,7 +86,7 @@ class AuthTest extends TestCase
     }
 
     /** @test */
-    public function it_loads_password_reset_request_form()
+    public function it_shows_password_reset_request_form()
     {
         $this->get('password/reset')
              ->assertStatus(200)
@@ -102,5 +105,51 @@ class AuthTest extends TestCase
         ]);
 
         Notification::assertSentTo($user, ResetPassword::class);
+    }
+
+    /** @test */
+    public function it_saves_password_reset_in_database()
+    {
+        $user = factory(User::class)->create();
+
+        Notification::fake();
+
+        $this->post('password/email', [
+            'email' => $user->email,
+        ]);
+
+        $this->assertDatabaseHas('password_resets', [
+            'email' => $user->email,
+        ]);
+    }
+
+    /** @test */
+    public function it_shows_password_reset_form()
+    {
+        $this->get('password/reset/dummy_token')
+             ->assertStatus(200)
+             ->assertViewIs('auth.passwords.reset');
+    }
+
+    /** @test */
+    public function it_redirects_to_sites_after_resetting_password()
+    {
+        $user = factory(User::class)->create();
+
+        $key = 'kS37T0FKPW74HVEtAedXR1Mub2/OTlHQ503Vn5Aaja8=';
+        $token = hash_hmac('sha256', Str::random(40), $key);
+        $hashedValue = app(BcryptHasher::class)->make($token);
+
+        DB::table('password_resets')->insert([
+                'email'      => $user->email,
+                'token'      => $hashedValue,
+        ]);
+
+        $this->post('password/reset', [
+            'token' => $token,
+            'email' => $user->email,
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ])->assertRedirect('sites');
     }
 }
