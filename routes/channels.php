500d20a00f295dfde902cde31f93266757089db6
Ben Wibrew <benjamin.wibrew@gmail.com>
2018-05-10 21:00:25 +0100

    Housekeeping

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+
 /*
 |--------------------------------------------------------------------------
 | Broadcast Channels
@@ -11,6 +13,6 @@
 |
 */
 
-Broadcast::channel('App.User.{id}', function ($user, $id) {
+Broadcast::channel('App.User.{id}', function (User $user, $id) {
     return (int) $user->id === (int) $id;
 });
